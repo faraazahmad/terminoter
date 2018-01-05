4142bf8040fa8185bfacba11e95625a6c6cc29c6
Syed Faraaz Ahmad <faraaz98@live.com>
2018-01-05 21:26:04 +0530

    add db class

--- db.py
+++ db.py
@@ -1,25 +1,35 @@
 # Use SQLite3 to handle the notes database
 import sqlite3
+
 # Use datetime library to insert date and time of
 # new note insertion to the database
 import datetime
 
-# create a Connect object that represents the database
-conn = sqlite3.connect('terminoter.db')
+class Db:
+    def __init__(self):
+        # create a Connect object that represents the database
+        self.conn = sqlite3.connect('terminoter.db')
 
-# create a cursor to handle the DB connection
-c = conn.cursor()
+        # create a cursor to handle the DB connection
+        self.c = conn.cursor()
+    
+    # Create the database table
+    def create_table():
+        self.c.execute('''create table notes
+            (date text, description text, user text)''')
 
-# Create the databse table
-def create_table():
-    c.execute('''create table notes
-        (date text, description text, user text)''')
+    def new_note(description):
+        now = datetime.datetime.now()
+        # change the time in text format for example:
+        # '2018-01-04 20:16'
+        datetime_string = now.strftime("%Y-%m-%d %H:%M")
 
-def new_note(description):
-    now = datetime.datetime.now()
-    # change the time in text format for example:
-    # '2018-01-04 20:16'
-    datetime_string = now.strftime("%Y-%m-%d %H:%M")
+        self.c.execute("""insert into notes
+            values (%s, %s)""", datetime_string, description)
 
-    c.execute("""insert into notes
-        values (?)""", datetime_string, description, )
+    def search_by_description(description):
+        t = (description, )
+        self.c.execute("select * from notes where description=?", t)
+
+    def close_conn():
+        self.c.close()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import platform
 import os
+import db
 
 notes_file_path = ''
 if platform.system() == 'Linux':
@@ -11,11 +12,16 @@ def main(args):
     for i in range(1, len(args)):
         if args[i] == '-n' or args[i] == '--new':
             new_note(args[i + 1])
+        elif args[i] == '-s' or args[i] == '--search'
+            search_by_description()
 
 def new_note(description):
-    notes_file = open(notes_file_path, 'a+')
-    notes_file.write(description + '\n')
-    notes_file.close()
+    db_conn = Db()
+    db_conn.new_note(description)
+    db_conn.close_conn()
+
+def search_note(description):
+    
 
 # def delete_note():
 
