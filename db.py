503739e76a92fb8619e3f9413fc0f7c05bf538b8
Syed Faraaz Ahmad <faraaz98@live.com>
2018-01-06 17:04:03 +0530

    add comments

--- db.py
+++ db.py
@@ -19,17 +19,28 @@ class Db:
             (date text, description text, user text)''')
 
     def new_note(description):
+        # get the current date and time
         now = datetime.datetime.now()
         # change the time in text format for example:
         # '2018-01-04 20:16'
         datetime_string = now.strftime("%Y-%m-%d %H:%M")
 
+        # insert the description along with the datetime
         self.c.execute("""insert into notes
             values (%s, %s)""", datetime_string, description)
 
     def search_by_description(description):
+        # create a tuple of description to avoid SQL injection
         t = (description, )
+        # select and return the record matching the description
         self.c.execute("select * from notes where description=?", t)
 
+    def delete_by_description(description):
+        # create a tuple of description to avoid SQL injection
+        t = (description, )
+        # delete the record that matches the description
+        self.c.execute("delete from notes where description=?", t)
+
+    # close the connection to the db
     def close_conn():
         self.c.close()
--- main.py
+++ main.py
@@ -1,28 +1,51 @@
-import platform
-import os
+# import the source file containing the db handling class
 import db
 
-notes_file_path = ''
-if platform.system() == 'Linux':
-    notes_file_path = os.path.join(
-        '/home', os.getlogin(), '.terminoter', 'notes'
-    )
+# imoprt sys library to access command line arguments
+import sys
 
+# search for argument in inputs and decide the action
+# to perform based on the argument, with the succeeding
+# list item as argument
 def main(args):
     for i in range(1, len(args)):
         if args[i] == '-n' or args[i] == '--new':
             new_note(args[i + 1])
-        elif args[i] == '-s' or args[i] == '--search'
-            search_by_description()
+        elif args[i] == '-s' or args[i] == '--search':
+            search_note(args[i + 1])
+        elif args[i] == '-d' or args[i] == '--delete':
+            delete_note(args[i + 1])
 
+# Create a new note in the DB using the description provided
 def new_note(description):
+    # create a new db connection cursor
     db_conn = Db()
+    # add the record in the DB
     db_conn.new_note(description)
+    # close the connection to the DB
     db_conn.close_conn()
 
+# search a record in the db matching the provided description
 def search_note(description):
-    
+    # create a new connection to the db
+    db_conn = Db
+    # store the result provided by sqlite and print it
+    result = db_conn.search_by_description(description)
+    print(result)
+    # TODO: handle no result case
 
-# def delete_note():
+    # close the connection to the db
+    db_conn.close_conn()
 
-main(platform.sys.argv)
+# delete a note from the DB where the description matches
+# the provided description
+def delete_note(description):
+    # create a new connection cursor to the db
+    db_conn = Db()
+    # delete the record that matches the description
+    db_conn.delete_by_description()
+    # close the connection to the DB
+    db_conn.close_conn()
+
+# call the driver function
+main(sys.argv)
